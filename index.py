fd32b59b365aa85f6ed3f6e69db298f9f6d3b2d3
syaifuddinh <syaifudinhadi8@gmail.com>
2024-01-01 21:10:54 +0800

    [EDIT] Make module callable

--- index.py
+++ index.py
@@ -12,44 +12,45 @@ from controllers import TestingController
 from sanic_cors import CORS, cross_origin
 
 
-load_dotenv()
+def application(environ, start_response):
+	load_dotenv()
 
-app = Sanic("CheckingGameApp")
-app.config.db = {
-	"name": os.environ.get("DB_NAME"),
-	"username": os.environ.get("DB_USER"),
-	"host": os.environ.get("DB_HOST"),
-	"password": os.environ.get("DB_PASSWORD"),
-	"port": os.environ.get("DB_PORT")
-}
+	app = Sanic("CheckingGameApp")
+	app.config.db = {
+		"name": os.environ.get("DB_NAME"),
+		"username": os.environ.get("DB_USER"),
+		"host": os.environ.get("DB_HOST"),
+		"password": os.environ.get("DB_PASSWORD"),
+		"port": os.environ.get("DB_PORT")
+	}
 
-@app.get("/")
-async def Index(request):
-	return json({
-		"isSuccess": True, 
-		"statusCode": 200,
-		"dbName": app.config.db["name"]
-	})
+	@app.get("/")
+	async def Index(request):
+		return json({
+			"isSuccess": True, 
+			"statusCode": 200,
+			"dbName": app.config.db["name"]
+		})
 
-@app.get("/test")
-async def Test(request):
-	return TestingController.Get(request)
+	@app.get("/test")
+	async def Test(request):
+		return TestingController.Get(request)
 
 
 
-Game.Init()
-Auth.Init()
+	Game.Init()
+	Auth.Init()
 
-CORS(app, resources={r"/*": {"origins": "*"}})
-# app.register_listener(setup_options, "before_server_start")
-# app.register_middleware(add_cors_headers, "response")
+	CORS(app, resources={r"/*": {"origins": "*"}})
+	# app.register_listener(setup_options, "before_server_start")
+	# app.register_middleware(add_cors_headers, "response")
 
-if __name__ == "__main__":
+	if __name__ == "__main__":
 
-	# Hot reload
+		# Hot reload
 
-    # reloader = LiveReloader()
-    # reloader.start_watcher_thread()
-    # end of Hot reload
+	    # reloader = LiveReloader()
+	    # reloader.start_watcher_thread()
+	    # end of Hot reload
 
-    app.run(host="0.0.0.0", port=3001)
+	    app.run(host="0.0.0.0", port=3001)
---
+++ testing.py
@@ -0,0 +1,6 @@
+import os
+
+import sys
+
+sys.path.append("/controllers")
+print("current directory : ", os.getcwd())
